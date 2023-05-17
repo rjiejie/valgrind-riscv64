06d3bb3ad77782560f81ae4c97a620d034ab82f5
Jojo R <rjiejie@linux.alibaba.com>
2023-05-17 14:23:07 +0800

    [T-HEAD] RVV: use t1/t2 register to emulate whole register insn

--- VEX/priv/host_riscv64V0p7_defs.c
+++ VEX/priv/host_riscv64V0p7_defs.c
@@ -37,7 +37,50 @@ static UChar* emit_RISCV64V0p7Instr(/*MB_MOD*/ Bool*    is_profInc,
                                     const void*         disp_cp_xindir,
                                     const void*         disp_cp_xassisted)
 {
-   return NULL;
+   UChar* p = buf;
+   switch (i->tag) {
+      case RISCV64in_VLdStWholeReg: {
+         Bool isLoad = i->RISCV64in.VLdStWholeReg.isLoad;
+         UInt sd     = vregEnc(i->RISCV64in.VLdStWholeReg.sd);
+         UInt base   = iregEnc(i->RISCV64in.VLdStWholeReg.base);
+         UChar m     = i->RISCV64in.VLdStWholeReg.m;
+         /*
+          csrr t1, vl
+          csrr t2, vtype
+          vsetvli x0, x0, e8, m1
+          vle/vse
+          vsetvl  x0, t1, t2
+         */
+         p = emit_I(p, OPC_SYSTEM, 6, RV64_SOPC_CSRRS, 0, 0xc20);
+         p = emit_I(p, OPC_SYSTEM, 7, RV64_SOPC_CSRRS, 0, 0xc21);
+         p = emit_I(p, OPC_OP_V,   0, RV64_SOPC_OPCFG, 0, 0);
+         UChar OPC = isLoad ? OPC_LOAD_FP : OPC_STORE_FP;
+         p = emit_I(p, OPC,      sd, 0b111, base, 0b000000100000);
+         p = emit_R(p, OPC_OP_V, 0,  RV64_SOPC_OPCFG, 6, 7, 0b1000000);
+         break;
+      }
+      case RISCV64in_VMV: {
+         UInt dst    = vregEnc(i->RISCV64in.VMV.dst);
+         UInt src    = vregEnc(i->RISCV64in.VMV.src);
+         UChar m     = i->RISCV64in.VMV.m;
+         /*
+          csrr t1, vl
+          csrr t2, vtype
+          vsetvli x0, x0, e8, m1
+          vmv
+          vsetvl  x0, t1, t2
+         */
+         p = emit_I(p, OPC_SYSTEM, 6, RV64_SOPC_CSRRS, 0, 0xc20);
+         p = emit_I(p, OPC_SYSTEM, 7, RV64_SOPC_CSRRS, 0, 0xc21);
+         p = emit_I(p, OPC_OP_V, 0,   RV64_SOPC_OPCFG, 0, 0);
+         p = emit_R(p, OPC_OP_V, dst, RV64_SOPC_OPIVV, src, 0, 0b0101111);
+         p = emit_R(p, OPC_OP_V, 0,   RV64_SOPC_OPCFG, 6,   7, 0b1000000);
+         break;
+      }
+      default:
+         return NULL;
+   }
+   return p;
 }
 
 static Bool getRegUsage_RISCV64V0p7Instr(HRegUsage* u, const RISCV64Instr* i)
