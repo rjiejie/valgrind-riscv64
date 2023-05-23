4ef1d7216bb878387371d1f72c32b0d8331a86a7
Jojo R <rjiejie@linux.alibaba.com>
2023-05-23 10:02:26 +0800

    [T-HEAD] RVV: add helpers file

---
+++ VEX/priv/guest_riscv64V0p7_helpers.c
@@ -0,0 +1,31 @@
+
+/*--------------------------------------------------------------------*/
+/*--- begin                             guest_riscv64V0p7_helpers.c --*/
+/*--------------------------------------------------------------------*/
+
+/*
+   This file is part of Valgrind, a dynamic binary instrumentation
+   framework.
+
+   Copyright (C) 2020-2022 T-HEAD of Alibaba
+      rjiejie@linux.alibaba.com
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+   The GNU General Public License is contained in the file COPYING.
+*/
+
+/*--------------------------------------------------------------------*/
+/*--- end                               guest_riscv64V0p7_helpers.c --*/
+/*--------------------------------------------------------------------*/
