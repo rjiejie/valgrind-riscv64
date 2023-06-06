abb10eb6ded1c04c1cf6d570a9a9cc4b260d700e
Jojo R <rjiejie@linux.alibaba.com>
2023-06-06 09:57:37 +0800

    [T-HEAD] RVV: Refine Single-Width Floating-Point/Integer Type-Convert Instructions

--- VEX/priv/guest_riscv64V0p7_toIR.c
+++ VEX/priv/guest_riscv64V0p7_toIR.c
@@ -237,24 +237,22 @@ static Bool dis_RV64V0p7_arith_OPF(/*MB_OUT*/ DisResult* dres,
 
       /* VFUnary0 */
       case 0b100010:
-         /*
-          * Single-Width Floating-Point/Integer Type-Convert Instructions
-          */
-         if (rs1 == 0b00000) {
-            GETC_VUnopOPF_V(vfcvt_xu_f);
-            return True;
-         } else if (rs1 == 0b00001) {
-            GETC_VUnopOPF_V(vfcvt_x_f);
-            return True;
-         } else if (rs1 == 0b00010) {
-            GETC_VUnopOPF_V(vfcvt_f_xu);
-            return True;
-         } else if (rs1 == 0b00011) {
-            GETC_VUnopOPF_V(vfcvt_f_x);
-            return True;
-         }
-
          switch (rs1) {
+            /*
+             * Single-Width Floating-Point/Integer Type-Convert Instructions
+             */
+            case 0b00000:
+               GETC_VUnopOPF_V(vfcvt_xu_f);
+               return True;
+            case 0b00001:
+               GETC_VUnopOPF_V(vfcvt_x_f);
+               return True;
+            case 0b00010:
+               GETC_VUnopOPF_V(vfcvt_f_xu);
+               return True;
+            case 0b00011:
+               GETC_VUnopOPF_V(vfcvt_f_x);
+               return True;
             /*
              * Widening Floating-Point/Integer Type-Convert Instructions
              */
